74f16dbc723be8f6f8b7b04a82d157bfc0ce5352
Lindsey Smith <lindseysmith1580@gmail.com>
2025-05-20 21:38:58 -0500

    changed project name

---
+++ scenic-spokes/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+import Header from './components/Header';
+
+function App() {
+
+  return (
+    <div id="window">
+      <Header />
+    </div>
+  )
+}
+
+export default App;
